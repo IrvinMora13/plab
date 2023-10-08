653e2d1e40e439420fe765877290f0db9304f928
Irvin Morales <a343423@gmail.com>
2023-10-07 18:42:07 -0600

    DynamixNavbar48

---
+++ planb/src/components/menuItems.jsx
@@ -0,0 +1,26 @@
+export const MenuItems = [
+    {
+        title: "Inicio",
+        url: "/",
+        cName: "navLinks",
+        icon: "fa-solid fa-house",
+    },
+    {
+        title: "Eventos",
+        url: "/Eventos",
+        cName: "navLinks",
+        icon: "",
+    },
+    {
+        title: "Carreras",
+        url: "/Carreras",
+        cName: "navLinks",
+        icon: "",
+    },
+    {
+        title: "Contacto",
+        url: "/Contactos",
+        cName: "navLinks",
+        icon: "fa-solid fa-info",
+    },
+]
--- planb/src/components/navBar.jsx
+++ planb/src/components/navBar.jsx
@@ -1,17 +1,25 @@
 import { Component } from "react";
 import { Link } from "react-router-dom";
 import "./navBar.css"
+import { MenuItems } from "./menuItems"
 
 class NavBar extends Component {
     render() {
         return (
-            <nav className="NavBar">
+            <nav className="NavBarItems">
                 <h1 className="NavBarLogo">UACH</h1>
                 <ul className="NavBarMenu">
-                    <li>
-                        <a>
-                            <i className="fa-solid fa-house"></i>Inicio</a>
-                    </li>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                    <i className={item.icon}>
+                                    </i>{item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+
                 </ul>
             </nav>
         )
