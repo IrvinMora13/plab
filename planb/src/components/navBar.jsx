2cdff9033aba1d45aa08607125fc63d59b023e74
Irvin Morales <a343423@gmail.com>
2023-10-07 19:59:57 -0600

    ResponsiveNavBarPhone

--- planb/src/components/navBar.jsx
+++ planb/src/components/navBar.jsx
@@ -4,11 +4,20 @@ import "./navBar.css"
 import { MenuItems } from "./menuItems"
 
 class NavBar extends Component {
+    state = { clicked: false }
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
     render() {
         return (
             <nav className="NavBarItems">
                 <h1 className="NavBarLogo">UACH</h1>
-                <ul className="NavBarMenu">
+
+                <div className="MenuIcons" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                </div>
+
+                <ul className={this.state.clicked ? "NavBarMenu active" : "NavBarMenu"}>
                     {MenuItems.map((item, index) => {
                         return (
                             <li key={index}>
